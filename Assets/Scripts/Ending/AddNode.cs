eb3021550ef94aa8b5088fe97d2339256d8c33c8
kys97 <kys971231@gmail.com>
2023-05-16 13:50:54 +0900

    엔딩 노드 프리펩 복구

--- Assets/Scripts/Ending/AddNode.cs
+++ Assets/Scripts/Ending/AddNode.cs
@@ -17,14 +17,17 @@ public class AddNode : MonoBehaviour
         CancelBtn = transform.Find("CancelBtn").gameObject.GetComponent<Button>();
         OkBtn = transform.Find("OKBtn").gameObject.GetComponent<Button>();
 
-        CancelBtn.onClick.AddListener(delegate
-        {
-
-        });
-
         OkBtn.onClick.AddListener(delegate
         {
-             
+             if(!IDIF.text.Equals("") && !NameIF.text.Equals(""))
+            {
+                //정보 이용해서 노드 추가
+                //기본 위치는 0,0,0
+                //NodeGroup Object 필요
+                //Prefab 필요
+                //Prefab에 들어갈 스크립트 필요
+                //스크립트에 들어갈 데이터 셋팅 필요
+            }
         });
     }
 
