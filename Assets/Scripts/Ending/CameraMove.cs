032fe508181a678859d162b067d542d0cc5749bd
kys97 <kys971231@gmail.com>
2023-05-21 16:50:59 +0900

    노드 이동

--- Assets/Scripts/Ending/CameraMove.cs
+++ Assets/Scripts/Ending/CameraMove.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,10 +7,15 @@ public class CameraMove : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
     Vector2 click, drag, move;
     Vector3 startposition;
-    [SerializeField] GameObject NodeGroup;
+    GameObject NodeGroup;
 
+    private void Start()
+    {
+        NodeGroup = GameObject.Find("EndingNodeGroup").gameObject;
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
+
         click = (Vector2)Camera.main.ScreenToWorldPoint(eventData.position);
 
         startposition = NodeGroup.transform.position;
